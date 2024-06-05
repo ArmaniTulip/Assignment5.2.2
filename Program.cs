8455763c7e93396797316c5b02da8f98ec522d2a
Garrett <Garrett@YUP>
2024-06-05 10:00:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter how many natural numbers to print");
+            NaturalNumbers(Convert.ToInt32(Console.ReadLine()));
+            
+        }
+
+        static void NaturalNumbers(int n)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                NaturalNumbers(n-1);
+                Console.WriteLine("" + n);
+            }
+        }
+        
+
+        
+
+
+        
+
+    }
+}
